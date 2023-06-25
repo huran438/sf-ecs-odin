674bf42b5d205ce033577bde104ef9235eddf46f
Ilya Khurani <huran438@gmail.com>
2023-06-25 05:06:59 +0300

    Update TransformRef.cs

--- Runtime/TransformRef.cs
+++ Runtime/TransformRef.cs
@@ -5,5 +5,8 @@ namespace SFramework.ECS.Runtime
     public struct TransformRef : ISFComponent
     {
         public Transform value;
+        public Vector3 position3 => value.position;
+        public Vector2 position2 => value.position;
+        public Quaternion rotation => value.rotation;
     }
 }
